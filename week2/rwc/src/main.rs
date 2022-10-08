1e50b421ae1d2688c6ac7c0e5b6a410f4526db68
jzwang <841713301@qq.com>
2022-10-08 16:28:24 +0800

    finish week2

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::process;
+use std::fs::File;
+use std::io::{self, BufRead};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,5 +10,19 @@ fn main() {
         process::exit(1);
     }
     let filename = &args[1];
-    // Your code here :)
+    let mut res = Vec::new();
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_chars = 0;
+    let file = File::open(filename).unwrap();
+    for line in io::BufReader::new(file).lines() {
+        let line_str = line.unwrap();
+        num_lines += 1;
+        num_words += line_str.split(' ').collect::<Vec<&str>>().len();
+        num_chars += line_str.len();
+        res.push(line_str);
+    }
+    println!("lines:{}", num_lines);
+    println!("words:{}", num_words);
+    println!("chars:{}", num_chars);
 }
